02eaee17408505796eff8724e9393e4eda80c3c6
Yaya <pricillia.mendes-freire@laplateforme.io>
2025-05-27 15:37:47 +0200

    pokedex fini

--- JavaScript/pokedex/script.js
+++ JavaScript/pokedex/script.js
@@ -1,15 +1,31 @@
 console.log("debut");
 const pokemonList = document.getElementById("list");
 const details = document.getElementById("details");
+const searchInput = document.getElementById("search");
+let pokemons = [];
 
+// requete pokebuild récup données json 
+
+// requête pokebuild récup données json 
 fetch("https://pokebuildapi.fr/api/v1/pokemon")
     .then(response => response.json())
-    .then(pokemons => {
+    .then(data => {
+        pokemons = data;  // Mets à jour la variable globale ici
         displayPk(pokemons);
         pokemons.forEach(pokemon => {
             console.log(pokemon.name);
         });
-    });
+});
+
+// barre de recherche
+searchInput.addEventListener("input", () => {
+    const search = searchInput.value.toLowerCase();
+    const found = pokemons.find(pokemon => pokemon.name.toLowerCase().includes(search));
+    if (found) {
+        pokemonSpec(found); // afficher détails du pokémon
+    }
+});
+
 
 function displayPk(pokemons) {
     pokemons.forEach(pokemon => {
@@ -19,19 +35,33 @@ function displayPk(pokemons) {
         div.innerHTML = `
         <span>${pokemon.id}</span>
         <span>${pokemon.name}</span>
-        <img src="${pokemon.image}" alt="${pokemon.name}"></img>`;
-        addEventListener("click", () => pokemonSpec(pokemon));
+        <img src="${pokemon.image}" alt="${pokemon.name}"></img>
+        `;
+        div.addEventListener("click", () => pokemonSpec(pokemon));
         pokemonList.appendChild(div);
     });
 
     function pokemonSpec(pokemon) {
         details.innerHTML = `
-        <p>${pokemon.id}</p>
+        <p>n°${pokemon.id}</p>
+        <img src="${pokemon.image}" alt="image du pokemon" />
         <h2>${pokemon.name}</h2>
-        <p>${pokemon}</p>
-
-
-        `
+        ${pokemon.apiTypes.map(type => `<img src="${type.image}" alt="${type.name}" />`).join("")}
+        
+        <div class="evolution">
+            <h3>Évolution</h3>
+            ${pokemon.apiEvolutions.length
+                ? pokemon.apiEvolutions.map(evo => `
+                    <div class="evolution-item" data-id="${evo.pokedexId}">
+                        <span>${evo.pokedexId}</span>
+                        <span>${evo.name}</span>
+                    </div>
+                `).join("")
+                : "<p>Aucune évolution</p>"
+            }
+        </div>
+    `;
     }
 
 }
+
